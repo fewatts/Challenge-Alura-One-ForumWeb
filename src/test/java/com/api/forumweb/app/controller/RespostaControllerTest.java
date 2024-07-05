77472cb1943add50d3033b0361631e9508394006
Fewatts <feralveswatts@gmail.com>
2024-07-05 14:08:17 -0300

    10º
    Configuração ambiente de testes

---
+++ src/test/java/com/api/forumweb/app/controller/RespostaControllerTest.java
@@ -0,0 +1,18 @@
+package com.api.forumweb.app.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+public class RespostaControllerTest {
+    
+    @Test
+    void getAll(){
+        
+    }
+
+}
